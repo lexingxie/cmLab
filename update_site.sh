67112d390b038dfdc4ce7e460ed16ccb72df3ac1
xlx <xlx@cecs>
2016-01-18 23:59:23 +1100

    minor update

--- update_site.sh
+++ update_site.sh
@@ -1,3 +1,9 @@
 git pull
+
+## note: must erase local public directory before generating,
+## otherwise old junk will be copied into /var/www/ !
+rm -rf public
+
 hugo -b http://cm.cecs.anu.edu.au
+
 cp -R public/* ~/cmlab
